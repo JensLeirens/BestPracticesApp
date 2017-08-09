83a9194ffcfd10d7afd823ebb6ef8e64bd3b0140
Mafken <jens.leirens@gmail.com>
2017-08-09 12:18:49 +0200

    add android and angular to the view add android and angular controllers add android and angular factory

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,12 +1,11 @@
 
-angular.module('flapperNews', ['ui.router'])
+angular.module('bestPractices', ['ui.router'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
+    $stateProvider.state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl',
@@ -15,8 +14,8 @@ function($stateProvider, $urlRouterProvider) {
           return posts.getAll();
         }]
       }
-    })
-    .state('posts', {
+    });
+    $stateProvider.state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
       controller: 'PostsCtrl',
@@ -25,8 +24,8 @@ function($stateProvider, $urlRouterProvider) {
           return posts.get($stateParams.id);
         }]
       }
-    })
-    .state('login', {
+    });
+    $stateProvider.state('login', {
       url: '/login',
       templateUrl: '/login.html',
       controller: 'AuthCtrl',
@@ -35,8 +34,8 @@ function($stateProvider, $urlRouterProvider) {
           $state.go('home');
         }
       }]
-    })
-    .state('register', {
+    });
+    $stateProvider.state('register', {
       url: '/register',
       templateUrl: '/register.html',
       controller: 'AuthCtrl',
@@ -49,6 +48,8 @@ function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('home');
 }])
+
+//all practices factory
 .factory('posts', ['$http', 'auth', function($http, auth){
   var o = {
     posts: []
@@ -90,6 +91,94 @@ function($stateProvider, $urlRouterProvider) {
 
   return o;
 }])
+
+//angular factory
+.factory('angular', ['$http', 'auth', function($http, auth){
+  var o = {
+    AngularPosts: []
+  };
+
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
+    });
+  };
+
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+
+  o.create = function(post) {
+    return $http.post('/posts', post, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      post.upvotes += 1;
+    });
+  };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    });
+  };
+
+  return o;
+}])
+
+//android factory
+.factory('android', ['$http', 'auth', function($http, auth){
+  var o = {
+    androidPosts: []
+  };
+
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
+    });
+  };
+
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+
+  o.create = function(post) {
+    return $http.post('/posts', post, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
+      post.upvotes += 1;
+    });
+  };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    });
+  };
+
+  return o;
+}])
+
+//user factory
 .factory('auth', ['$http', '$window', '$rootScope', function($http, $window, $rootScope){
    var auth = {
     saveToken: function (token){
@@ -134,7 +223,61 @@ function($stateProvider, $urlRouterProvider) {
 
   return auth;
 }])
+
 .controller('MainCtrl', [
+'$scope',
+'posts',
+'angular',
+'android',
+'auth',
+function($scope, posts, angular, android, auth){
+
+  $scope.posts = posts.posts;
+  $scope.posts = $scope.posts.concat(angular);
+  $scope.posts = $scope.posts.concat(android);
+
+  $scope.isLoggedIn = auth.isLoggedIn;
+
+  $scope.addPost = function(){
+    if($scope.title === '') { return; }
+    posts.create({
+      title: $scope.title,
+      link: $scope.link,
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    posts.upvote(post);
+  };
+
+}])
+.controller('androidCtrl', [
+'$scope',
+'posts',
+'auth',
+function($scope, posts, auth){
+
+  $scope.posts = posts.posts;
+  $scope.isLoggedIn = auth.isLoggedIn;
+
+  $scope.addPost = function(){
+    if($scope.title === '') { return; }
+    posts.create({
+      title: $scope.title,
+      link: $scope.link,
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    posts.upvote(post);
+  };
+
+}])
+.controller('angularCtrl', [
 '$scope',
 'posts',
 'auth',
